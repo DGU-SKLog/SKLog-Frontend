aed758d8e1d40ec84c132a6c219c381ebc1f35e2
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-09 16:34:29 +0900

    Fix: api content

--- src/components/SelectModal/index.tsx
+++ src/components/SelectModal/index.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react'
 import { Root, SelectItem } from './styled'
 import { SelectItemList } from 'constants/selectItemList'
 import { RequestModal } from 'components/RequestModal'
+import { CommonRequestProps } from 'api/common/commonType'
 type SelectModalProps = {
   closeModal: () => void
   content: string
@@ -10,8 +11,8 @@ type SelectModalProps = {
 export const SelectModal: FC<SelectModalProps> = ({ content, closeModal }) => {
   const selectItemList = SelectItemList
   const [isRequestModalOpen, setIsRequestModalOpen] = useState(false)
-  const onSelectItemClick = (api: any) => () => {
-    api().then((res) => {
+  const onSelectItemClick = (api: any, req: CommonRequestProps) => () => {
+    api(content).then((res) => {
       console.log(res)
       setIsRequestModalOpen(true)
     })
@@ -27,7 +28,7 @@ export const SelectModal: FC<SelectModalProps> = ({ content, closeModal }) => {
   return (
     <Root onClick={onClickModal}>
       {selectItemList.map((item, index) => (
-        <SelectItem key={index} src={item.uri} onClick={onSelectItemClick(item.api)} />
+        <SelectItem key={index} src={item.uri} onClick={onSelectItemClick(item.api, { content: content })} />
       ))}
       {isRequestModalOpen && <RequestModal closeModal={closeModalAll} content={content} />}
     </Root>
