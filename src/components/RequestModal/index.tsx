af54f525adef612a72107a9ee9f3ab5eef39e207
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-11-11 16:13:23 +0900

    Feat: Request API

--- src/components/RequestModal/index.tsx
+++ src/components/RequestModal/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, FC, useEffect, useRef, useState } from 'react'
 import {
   ApplyButton,
   AskImg,
@@ -17,6 +17,7 @@ import { ModalWrapper } from 'components/common/commonStyle'
 import AskIcon from 'assets/images/ask_icon.svg'
 import ReactMarkDown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import { CreateRequestResponseProps, createRequest } from 'api/createRequest'
 type RequestModalProps = {
   closeModal: () => void
   content: string
@@ -24,13 +25,22 @@ type RequestModalProps = {
 }
 
 export const RequestModal: FC<RequestModalProps> = ({ closeModal, content, applyAIText }) => {
+  const [inputValue, setInputValue] = useState<string>('')
   const onClickApplyButton = () => {
     // 적용 api
     applyAIText(apiResponse)
     closeModal()
   }
   const onClickClearButton = () => {
-    //원래 상태로 초기화
+    setInputValue('')
+  }
+  const onClickRequestButton = () => {
+    createRequest({ content: content, request: inputValue }).then((res: CreateRequestResponseProps) => {
+      setApiResponse(res.content)
+    })
+  }
+  const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(event.target.value)
   }
   const onClickModal = (e: React.MouseEvent) => {
     e.stopPropagation()
@@ -48,8 +58,13 @@ export const RequestModal: FC<RequestModalProps> = ({ closeModal, content, apply
       <Root onClick={onClickModal}>
         <UpperContainer>
           <AskImg src={AskIcon} />
-          <RequestInput placeholder="AI에게 요청할 내용을 입력하세요!" ref={inputRef} />
-          <RequestButton>요청</RequestButton>
+          <RequestInput
+            placeholder="AI에게 요청할 내용을 입력하세요!"
+            ref={inputRef}
+            value={inputValue}
+            onChange={onChange}
+          />
+          <RequestButton onClick={onClickRequestButton}>요청</RequestButton>
         </UpperContainer>
         <CenterContainer>
           <LeftContainer>
@@ -61,7 +76,7 @@ export const RequestModal: FC<RequestModalProps> = ({ closeModal, content, apply
             <ButtonContainer>
               <ApplyButton onClick={onClickApplyButton}>적용</ApplyButton>
               <CancelButton onClick={closeModal}>취소</CancelButton>
-              <ClearButton>새 질문</ClearButton>
+              <ClearButton onClick={onClickClearButton}>새 질문</ClearButton>
             </ButtonContainer>
           </RightContainer>
         </CenterContainer>
